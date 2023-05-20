84b69c754b49017182b46d0653db8c4fdc0948ab
sabi1125 <sabir_barahi4@hotmail.com>
2023-05-21 08:48:30 +0900

    added: added users and user_metrics tables

---
+++ db/migrations/000002_create_users_metrics_table.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS user_metrics(
+    user_metrics_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    height INT(3) NOT NULL,
+    weight INT(3) NOT NULL,
+    age INT(3) NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    bmi INT(3) NOT NULL,
+    bmr INT(4) NOT NULL, -- BMR is the calorie needed to sustain basic bodily functions at rest
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT check_height_range CHECK (height BETWEEN 54 AND 280),
+    CONSTRAINT check_weight_range CHECk (weight BETWEEN 2 AND 635),
+    CONSTRAINT check_age_range CHECK (age BETWEEN 0 AND 125),
+    CONSTRAINT check_bmi_range CHECK (bmi BETWEEN 6 AND 105),
+    PRIMARY KEY(user_metrics_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
